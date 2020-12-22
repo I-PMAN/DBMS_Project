962cd1060b9f1c6fa0278b4ba00b44a868f1f473
I-PMAN <hero.parth@gmail.com>
2020-12-22 10:50:10 +0530

    Creating an AdminPage, relocating php files.

--- UserLogQ.php
+++ UserLogQ.php
@@ -1,18 +1,15 @@
 <?php
-session_start();
 require "dbcon.php";
 $username = filter_input(INPUT_POST, 'username');
 $password = filter_input(INPUT_POST, 'password');
-
-//$hash_pass=password_hash($password,PASSWORD_DEFAULT);
-
+ 
 $query = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
 if(mysqli_num_rows($query) > 0){
   $row = mysqli_fetch_assoc($query);
   $user_db_pass = $row['pword'];
   if($password === $user_db_pass){
     session_regenerate_id();
-    $_SESSION['cookies_name'] =  $row['username'];  
+    $_SESSION["username"] = $username;  
     header('Location: test1.php');
     exit;
   }
