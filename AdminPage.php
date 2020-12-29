0f2557b41353cef3a7e6c878970fe3343657f28b
shreyask22 <69723018+shreyask22@users.noreply.github.com>
2020-12-29 09:28:52 +0530

    everything working, trigger and ticket remaining (#12)

--- AdminPage.php
+++ AdminPage.php
@@ -2,6 +2,7 @@
 session_start();
 ?>
 <!DOCTYPE html>
+<?php include('Admin_navbar1.html'); ?>
 <html lang="en" dir="ltr">
 <link rel="stylesheet" href="styles.css">
   <head>
@@ -13,18 +14,11 @@ session_start();
     <h3 id="Adm_Wel">Welcome, Admin.</h3>
     <div class="container">
       <div class="left">
-        <a href="AddMovie.php">
-        <button type="button" name="button" class="button button1">Add a new Movie</button>
-        </a>
+        <button type="button" name="button" class="button button1"><a href="AddMovie.php" style="text-decoration:none;">Add a new Movie</a></button>
       </div>
-      <a href="">
       <div class="right">
-        <button type="button" name="button" class="button button2">Remove a Movie</button>
-        </a>
+        <button type="button" name="button" class="button button2"><a href="RemoveMovie.php" style="text-decoration:none;">Remove a Movie</a></button>
         </div>
-     </div>
-     <div class="Movie_details"
-
      </div>
     </div>
   </body>
--- movie_select.php
+++ movie_select.php
@@ -9,16 +9,14 @@ while($row = mysqli_fetch_array($sql)){
 ?>
 
 <html>
+<?php include('navbar1.html'); ?>
 <link rel="stylesheet" href="styles.css">
 <body>
-<div class="form_display" id="grad">
+
+<div class="form_display" id="grad" style="float:left">
 <form action="" method="POST" id="movie_form">
-
-<div>
- <input type="date" name="date_select" style="margin:10px">
-</div>
-
-<div>
+<input type="date" name="date_select" style="margin:10px" class="desc_submit">
+<br>
 <select name="time_select" required>
 <option value="choose time">[Choose Time]</option>
 <option value="10:00:00"><time>10:00AM [10:00]</time></option>
@@ -27,9 +25,7 @@ while($row = mysqli_fetch_array($sql)){
 <option value="19:00:00"><time>07:00PM [19:00]</time></option>
 <option value="22:00:00"><time>10:00PM [22:00]</time></option>
 </select>
-</div>
-
-<div>
+<br>
  <select name="movie_select">
  <option value="No movie is selected">[Choose Movie below]</option>
  <?php
@@ -38,9 +34,7 @@ while($row = mysqli_fetch_array($sql)){
  }
  ?>
 </select>
-</div>
-
-<div>
+<br>
 <select name="seat_select" required>
 <option value="No. of seats not selected">[Choose no. of seats]</option>
 <option value="1">1</option>
@@ -53,11 +47,12 @@ while($row = mysqli_fetch_array($sql)){
 <option value="8">8</option>
 <option value="9">9</option>
 <option value="10">10</option>
-</select>
-<div>
-<input type="submit" value="select" class="submit">
+</select> 
+<br>
+<input type="submit" value="select" class="submit" style="border-radius: 10px;background: rgba(255, 255, 255, 0.2);">
 </form>
 </div>
+<br>
 <?php
 // ###getting selected values from form###
 $date_select = filter_input(INPUT_POST, 'date_select');
@@ -67,15 +62,14 @@ $seat_select = filter_input(INPUT_POST, 'seat_select');
 // ###Mandatory to place this here###
 ?>
 <!-- collecting data from user and keepting them in session variables -->
-<div class="show_display">
+<div class="show_display" style="float:right">
 <h4>Selection will be displayed here</h4>
 <h4>Movie: <?php echo $movie_select; $_SESSION["session_movie"] = $movie_select; ?> </h4>
 <h4>Seats: <?php echo $seat_select; $_SESSION["session_seat"] = $seat_select;  ?> </h4>
 <h4>Date: <?php echo $date_select; $_SESSION["session_date"] = $date_select;  ?> </h4>
 <h4>Time: <?php echo $time_select; $_SESSION["session_time"] = $time_select;  ?> </h4>
-</div>
-<div>
-<input type="button" value="Book" onclick="book_ticket()" href="movie_selectQ.php">
+<br>
+<input type="button" value="Book" onclick="book_ticket()" href="movie_selectQ.php" style="border-radius: 10px;background: rgba(255, 255, 255, 0.2);">
 </div>
 <!--checking if all fileds are selected -->
 <?php if(!empty($seat_select) && !empty($movie_select) && !empty($date_select) && !empty($time_select))
